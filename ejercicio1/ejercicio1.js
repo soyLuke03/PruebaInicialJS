47a00763490be7659a4ad36eb81cc6f1392126f2
Luke <99337120+soyLuke03@users.noreply.github.com>
2022-09-19 12:23:03 +0200

    Add files via upload

--- ejercicio1/ejercicio1.js
+++ ejercicio1/ejercicio1.js
@@ -8,28 +8,32 @@ let nombre;
 
 nombre = prompt("Dame un nombre");   
 
-    lista.innerText += nombre + " , ";
-    arrayNombres.push(nombre);
+  
+    arrayNombres.push(nombre.charAt(0).toUpperCase());
 
 do{
 
 
     
-    nombre = prompt("Dame un nombre");   
+    nombre = prompt("Dame un nombre");  
+   
 
     if(arrayNombres.indexOf(nombre) != -1){
         alert("no se puede");
     }
     else {
 
-    
-    lista.innerText += nombre + " , ";
-    arrayNombres.push(nombre);
+    arrayNombres.push(nombre.charAt(0).toUpperCase());
     }
 
 }
 while(
     arrayNombres.length < 5
 );
+
+arrayNombres.sort();
+
+
+lista.innerText = arrayNombres;
 
 }
